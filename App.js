1fc0da65b63beaed57f31ac23c29708021e1732c
adidarali <alid.didarali@keytree.co.uk>
2018-06-02 22:40:37 +0100

    test for eslint error
    test1

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/common/header';
-import Login from './src/components/pages/Login';
+import Main from './src/components/Main';
+// import Header from './src/components/common/header';
+// import Login from './src/components/pages/Login';
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-          <Header headerText={'Holiday Plan!'} />
-          <Login />
-      </View>
+          <Main />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
