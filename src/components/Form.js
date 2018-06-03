2e4b5634603ccd409f17813a32dc15d602783980
adidarali <alid.didarali@keytree.co.uk>
2018-06-03 13:54:29 +0100

    remove unwanted code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import Dimensions from 'Dimensions';
+//import PropTypes from 'prop-types';
+//import Dimensions from 'Dimensions';
 import {
   StyleSheet,
   KeyboardAvoidingView,
-  View,
-  ActivityIndicator,
   TouchableOpacity,
   Image,
 } from 'react-native';
 
 import UserInput from './UserInput';
-import ButtonSubmit from './ButtonSubmit';
-import SignupSection from './SignupSection';
 
 import usernameImg from '../images/username.png';
 import passwordImg from '../images/password.png';
@@ -63,8 +59,8 @@ export default class Form extends Component {
   }
 }
 
-const DEVICE_WIDTH = Dimensions.get('window').width;
-const DEVICE_HEIGHT = Dimensions.get('window').height;
+// const DEVICE_WIDTH = Dimensions.get('window').width;
+// const DEVICE_HEIGHT = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
