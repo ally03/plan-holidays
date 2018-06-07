518c82aa77a2b97b61fdf53409f1fc2c3cfdee69
adidarali <alid.didarali@keytree.co.uk>
2018-06-07 17:55:08 +0100

    fb login auth

--- src/components/LoginPage/LoginComp/fbLogin.js
+++ src/components/LoginPage/LoginComp/fbLogin.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
-import Expo from 'expo';
 import {
   StyleSheet,
   View,
   Text,
   TouchableHighlight,
-  Alert
+  AsyncStorage
+//   Alert
 } from 'react-native';
 //import { Button } from 'react-native-elements';
+//import Expo from 'expo';
+import { connect } from 'react-redux';
+import * as actions from '../../../actions';
+import { Actions } from 'react-native-router-flux';
 
-export default class FbLogin extends Component {
-
-    async logInFB() {
-        const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('760101284378819', {
-            permissions: ['public_profile'],
-          });
-        if (type === 'success') {
-          // Get the user's name using Facebook's Graph API
-          const response = await fetch(
-            `https://graph.facebook.com/me?access_token=${token}`);
-          Alert.alert(
-            'Logged in!',
-            `Hi ${(await response.json()).name}!`,
-          );
-        }
+class FbLogin extends Component {
+    constructor(props) {
+        super(props);
+        this._onPressButton = this._onPressButton.bind(this);    
+    }
+    // componentDidMount() {
+    //     this.props.facebookLogin();
+    // }
+    // async logInFB() {
+    //     const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('760101284378819', {
+    //         permissions: ['public_profile'],
+    //       });
+    //     if (type === 'success') {
+    //       // Get the user's name using Facebook's Graph API
+    //       const response = await fetch(
+    //         `https://graph.facebook.com/me?access_token=${token}`);
+    //       Alert.alert(
+    //         'Logged in!',
+    //         `Hi ${(await response.json()).name}!`,
+    //       );
+    //     }
+    //}
+    
+    _onPressButton() {
+        this.props.facebookLogin()
+        //Actions.secondScreen();
+        //AsyncStorage.removeItem('fb_token');
     }
 
-    render() {
+
+    render() { 
         return (
             <View style={styles.container}>
-                <TouchableHighlight onPress={this.logInFB} underlayColor="white">
+                <TouchableHighlight onPress={this._onPressButton}>
                     <View style={styles.button}>
                         <Text style={styles.buttonText}>Login with Facebook</Text>
                     </View>
@@ -56,3 +73,7 @@ export default class FbLogin extends Component {
       color: 'white'
     }
   });
+
+
+
+  export default connect(null, actions) (FbLogin);
